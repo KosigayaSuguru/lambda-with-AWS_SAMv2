222715fd56bcd156f3160a5a8fea3c93d36c789c
EC2 Default User <ec2-user@ip-172-31-45-126.ap-northeast-1.compute.internal>
2020-09-25 20:01:29 +0000

    datasetでSQLを実行する部分を色々改修

--- src/lambda_functions/function_db_dataset/fn/app.py
+++ src/lambda_functions/function_db_dataset/fn/app.py
@@ -7,15 +7,25 @@ sys.path.append('/opt')  # nopep8
 
 def lambda_handler(event, context):
 
-    import threading
-    threads = []
-    for idx in range(10):
-        t = threading.Thread(name="thread1", target=exec_dataset)
-        t.start()
-        threads.append(t)
+    from .db import con
 
-    for thread in threads:
-        thread.join()
+    # スレッドでSQL実行
+    thread_run_exec_dataset(10)
+
+    # # 普通にSQL実行
+    # exec_dataset()
+
+    close_and_monitor(con)
+    wait_and_countdown(5)
+
+    # スレッドでSQL実行
+    thread_run_exec_dataset(10)
+
+    # # 普通にSQL実行
+    # exec_dataset()
+
+    close_and_monitor(con)
+    wait_and_countdown(5)
 
     return {
         "statusCode": 200,
@@ -27,7 +37,45 @@ def lambda_handler(event, context):
 
 def exec_dataset():
     from .db import con
-    results = con.query('select count(*) as cnt from test1 as t1,test1 as t2, test1 as t3, test1 as t4, test1 as t5, test1 as t6;')
+    results = con.query(
+        'select count(*) as cnt from ' +
+        'test1 as t1, test1 as t2, test1 as t3, test1 as t4, test1 as t5, ' +
+        'test1 as t6, test1 as t7, test1 as t8, ' +
+        'test1 as t100;'
+    )
+
     for record in results:
         print(record['cnt'])
-    # con.close() # conをclose()すると動かなくなる。。
+    # con.close()  # conをclose()すると動かなくなる。。
+
+
+def thread_run_exec_dataset(threads):
+    import threading
+    thread_list = []
+    for idx in range(threads):  # threadsはスレッド数
+        t = threading.Thread(name="thread1", target=exec_dataset)
+        t.start()
+        thread_list.append(t)
+
+    for thread in thread_list:
+        thread.join()
+
+
+def wait_and_countdown(sec):
+    for idx in range(sec):
+        import time
+        time.sleep(1)
+        print(idx)
+
+
+def close_and_monitor(con):
+    print(f"con.executable.closed     : {con.executable.closed}")
+    print(f"con.executable.invalidated: {con.executable.invalidated}")
+    print(f"con.engine.pool.status()  : {con.engine.pool.status()}")
+    # con.close()
+    # con.executable.close()
+    # con.executable.invalidate()
+    # con.executable.engine.dispose()
+    print(f"con.executable.closed     : {con.executable.closed}")
+    print(f"con.executable.invalidated: {con.executable.invalidated}")
+    print(f"con.engine.pool.status()  : {con.engine.pool.status()}")
--- src/lambda_functions/function_db_dataset/fn/db.py
+++ src/lambda_functions/function_db_dataset/fn/db.py
@@ -6,9 +6,13 @@ import dataset
 
 from sqlalchemy.pool import NullPool
 
-con = dataset.connect('mysql://root:password@host.docker.internal:33306/hoge',
-                      engine_kwargs={
-                          # 'poolclass': NullPool, # コネクションプールを使いたくない時
-                          'pool_size': 10,
-                      }
-                      )
+con = dataset.connect(
+    'mysql://root:password@172.17.0.1:3306/hoge',
+    engine_kwargs={
+        # 'poolclass': NullPool, # コネクションプールを使いたくない時
+        'pool_size': 5,
+        'max_overflow': 5,  # 0以外を指定すると、pool_sizeを超えてくるので注意
+        # 'pool_recycle': 2,
+        'echo': True,
+    }
+)
